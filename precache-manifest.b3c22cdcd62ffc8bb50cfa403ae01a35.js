c9da84963316bd82f3eda73431ca6b40714356d2
Rahul Patel <rahul_pat@outlook.com>
2020-09-06 10:32:09 -0400

    Updates

---
+++ precache-manifest.b3c22cdcd62ffc8bb50cfa403ae01a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d33f92b055b26be18a252892fd6eafe9",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "357809d1ed72f77d12f7",
+    "url": "/expense-tracker/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "28968ee6902e264a5283",
+    "url": "/expense-tracker/static/css/main.9967f66b.chunk.css"
+  },
+  {
+    "revision": "357809d1ed72f77d12f7",
+    "url": "/expense-tracker/static/js/2.62b389c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/expense-tracker/static/js/2.62b389c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28968ee6902e264a5283",
+    "url": "/expense-tracker/static/js/main.395dd7fc.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
